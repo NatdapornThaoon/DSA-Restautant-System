80b2d3733950785b0e3b361ea73cc9686d4b1f02
Natda <6831503043@lamduan.mfu.ac.th>
2026-04-07 18:30:54 +0700

    Fist commit

---
+++ src/main/java/DSA/MainApp.java
@@ -0,0 +1,5 @@
+package main.java.DSA;
+
+public class MainApp {
+    
+}
---
+++ src/main/java/models/OrderItem.java
@@ -0,0 +1,21 @@
+package main.java.models;
+
+public class OrderItem {
+
+    private final MenuItems item;
+    private final int quantity;
+
+    public OrderItem(MenuItems item,int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public double  getTotal() {
+        return item.getPrice()*quantity;
+    }
+
+    @Override
+    public String toString() {
+        return item.getName() + "x" + quantity + " = " +getTotal();
+    }
+}
